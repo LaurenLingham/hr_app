f2114739c04c6b17436149e27c72f7c870ce34ec
LaurenLingham <lauren.lingham@outlook.com>
2023-08-17 10:54:33 +0100

    added employee model and updated employee service

--- client/src/app/services/employee.service.ts
+++ client/src/app/services/employee.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  public employees!: Observable<Employee[]>;
 
   constructor() { }
 }
